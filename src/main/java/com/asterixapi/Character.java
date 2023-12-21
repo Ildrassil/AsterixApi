4744b85dba377a20dd002f7c0f28718b145f50e4
Ildrassil <schneiderjakob7@googlemail.com>
2023-12-21 12:35:47 +0100

    isnt running properly

--- src/main/java/com/asterixapi/Character.java
+++ src/main/java/com/asterixapi/Character.java
@@ -1,20 +1,13 @@
 package com.asterixapi;
 
-import lombok.NoArgsConstructor;
-import lombok.With;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Field;
-
-
-@With
 public record Character(
-        @Field
+
         String id,
-        @Field
+
         String name,
-        @Field
+
         int age,
-        @Field
+
         String profession
 
 ) {
--- src/main/java/com/asterixapi/CharacterRepo.java
+++ src/main/java/com/asterixapi/CharacterRepo.java
@@ -1,7 +1,4 @@
 package com.asterixapi;
-
-
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.ArrayList;
@@ -10,8 +7,6 @@ import java.util.List;
 public interface CharacterRepo extends MongoRepository<Character, String> {
     List<Character> characters = new ArrayList<>();
 
-
-    //default List<Character> find(Query query, Class<Character> userClass){
 
     }
 
